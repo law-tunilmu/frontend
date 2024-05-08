bb0729660de7ab86b8ae93825e4255c0a02155b9
krzki <kurniawanwawan1707@gmail.com>
2024-05-08 09:52:50 +0700

    misc: remove app module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Login from './auth/login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
