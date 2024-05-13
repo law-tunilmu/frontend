2a7649c473e85e01c6fd58a215beb196657d4f62
krzki <kurniawanwawan1707@gmail.com>
2024-05-13 11:25:42 +0700

    revise dashboard layout

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,9 +6,8 @@ export default function Dashboard() {
     const data = batchCourseLoader({page_size:dataLen});
     return (
         <div className="container w-11/12 sm:w-4/5 mx-auto min-h-screen mt-2">
-            <div className="flex flex-col w-full gap-1">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full gap-1">
                 {[...data.map((course) => <CourseMiniView courseHeaderData={course}/>)]}
-                <div className="basis-1/5">page no</div>
             </div>
         </div>
     );
--- src/course/CourseMiniView.js
+++ src/course/CourseMiniView.js
@@ -3,21 +3,25 @@ import { Link } from "react-router-dom";
 export function CourseMiniView({courseHeaderData}) {
     const data = courseHeaderData;
     return (
-        <div key={data.id} className="border-2 border-solid flex flex-row gap-[0.5rem] max-h-[12rem] px-2 py-2">
-            <div className="basis-2/5 lg:basis-1/5">
-                <img 
-                    src="https://res.cloudinary.com/dwlvcqj0u/image/upload/v1696150547/cld-sample-5.jpg"
-                    className="w-full h-full" 
-                    alt="a picture"
-                />
-            </div>
-            <div className="grid grid-rows-7 grid-cols-1 basis-3/5 lg:basis-4/5 text-sm sm:text-md">
-                <div className="font-bold text-wrap truncate">
-                    <Link to={`course/${data.id}`} className="text-blue-500"><h2>{data.title}</h2></Link>
-                </div>
-                <div className="w-4/5 text-wrap truncate"><a href="#" className="text-green-700">{data.creator}</a></div>
-                <div className="flex flex-row items-center"><span className="font-bold">$ {data.price}</span></div>
-                <div className="row-span-4 text-wrap truncate text-justify relative">
+        <div key={data.id} className="border-2 border-solid flex flex-col gap-[0.5rem] px-2 py-2 w-full">
+            <img 
+                src="https://res.cloudinary.com/dwlvcqj0u/image/upload/v1696150547/cld-sample-5.jpg"
+                className="w-full object-scale-down h-64 sm:h-80" 
+                alt="Course Image"
+            />
+            
+            <div className="grid grid-cols-1 text-sm sm:text-md gap-2">
+                <Link to={`course/${data.id}`}>
+                    <p className="font-bold break-words text-blue-500 line-clamp-3">
+                        {data.title}
+                    </p>
+                </Link>
+
+                <p href="#" className="text-green-700 line-clamp-1 pr-1">
+                    <strong className="text-black">created by </strong>{data.creator}
+                </p>
+                <p className="font-bold">$ {data.price}</p>
+                <div className="text-justify relative line-clamp-6">
                     {data.description}
                     <Link to={`course/${data.id}`}>
                         <div className="absolute bottom-0 right-0 z-1 
